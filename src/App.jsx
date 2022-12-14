da42f1f6ed97f3a4f496fb3714fd5c91ee3d7c41
Micha Alt <mail@lean-stack.de>
2022-12-14 21:19:48 +0100

    Install and configure tailwindcss.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Lean React</h1>;
+  return (
+    <header className="flex items-center px-8 sm:px-4 bg-white shadow">
+      <h1 className="py-4 text-2xl font-semibold">Lean React</h1>
+    </header>
+  );
 }
 
 export default App;
